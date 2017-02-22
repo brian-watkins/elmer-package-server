033c4e65573c2a987ff462c638064564bf959f47
Brian Watkins <brian.watkins@gmail.com>
2017-02-21 20:29:30 -0500

    Deploy license with executable

--- scripts/deployBin.sh
+++ scripts/deployBin.sh
@@ -3,20 +3,29 @@
 set -xe
 
 VERSION=0.1
-LINUX_BIN=elmer-package-${VERSION}-linux.zip
-MACOS_BIN=elmer-package-${VERSION}-darwin.zip
+LINUX_DIST=elmer-package-${VERSION}-linux.tar.gz
+MACOS_DIST=elmer-package-${VERSION}-darwin.tar.gz
+DIST_DIR=./elmer-package-${VERSION}
 
 pushd linux-bin
 
-zip ${LINUX_BIN} ./elmer-package
+mkdir ${DIST_DIR}
+cp ../elm-package/LICENSE ${DIST_DIR}
+cp elmer-package ${DIST_DIR}
+tar -cvzf ${LINUX_DIST} ${DIST_DIR}
+rm -rf ${DIST_DIR}
 
 popd
 
 pushd macos-bin
 
-zip ${MACOS_BIN} ./elmer-package
+mkdir ${DIST_DIR}
+cp ../elm-package/LICENSE ${DIST_DIR}
+cp elmer-package ${DIST_DIR}
+tar -cvzf ${MACOS_DIST} ${DIST_DIR}
+rm -rf ${DIST_DIR}
 
 popd
 
-mv ./linux-bin/${LINUX_BIN} ./app/public/downloads/
-mv ./macos-bin/${MACOS_BIN} ./app/public/downloads/
+mv ./linux-bin/${LINUX_DIST} ./app/public/downloads/
+mv ./macos-bin/${MACOS_DIST} ./app/public/downloads/
