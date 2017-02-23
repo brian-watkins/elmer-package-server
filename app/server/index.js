d3712d46fae9d15499cd47a65935a6926a4a03f1
Brian Watkins <brian.watkins@gmail.com>
2017-02-22 21:46:28 -0500

    Page views are better associated with a user
    - use session tracking to associate documentation
    views with the user as identified by the ga 
    cookie

--- app/server/index.js
+++ app/server/index.js
@@ -1,18 +1,17 @@
 var express = require('express');
 var app = express();
 var ua = require('universal-analytics');
+var cookieSession = require('cookie-session');
 const pathUtil = require('path');
 
 module.exports = function (elmer_versions) {
 
-  var sendAnalyticsEvent = function (category, action) {
-    var visitor = ua('UA-92414229-1');
-    visitor.event(category, action).send();
+  var sendAnalyticsEvent = function (req, category, action) {
+    req.visitor.event(category, action).send();
   }
 
-  var sendPageView = function (path) {
-    var visitor = ua('UA-92414229-1');
-    visitor.pageview(path).send();
+  var sendPageView = function (req, path) {
+    req.visitor.pageview(path).send();
   }
 
   var staticPagePath = function(fullPath) {
@@ -21,18 +20,34 @@ module.exports = function (elmer_versions) {
     return fullPath.substr(start + delim.length);
   }
 
-  app.use(express.static('public', { setHeaders: function (res, path, stat) {
+  app.use(cookieSession({
+    name: 'elmer-test-session',
+    keys: ['dfsdkhfdsjk7868', 'hsdh535vjsbd7s']
+  }))
+
+  app.use(ua.middleware("UA-92414229-1", {cookieName: '_ga'}));
+
+  app.use("/packages", function(req, res, next) {
+    if (pathUtil.extname(req.path) == '') {
+      sendPageView(req, req.path);
+    }
+    next();
+  });
+
+  app.use("/downloads", function(req, res, next) {
+    sendAnalyticsEvent(req, "Download", pathUtil.basename(req.path));
+    next();
+  });
+
+  app.use(express.static('public', { setHeaders: function (response, path, stat) {
       if (pathUtil.extname(path) == '') {
-        sendPageView(staticPagePath(path));
-        res.set('Content-Type', 'text/html');
-      } else if (pathUtil.extname(path) == '.gz') {
-        sendAnalyticsEvent("Download", pathUtil.basename(path));
+        response.set('Content-Type', 'text/html');
       }
     }
   }));
 
   app.get('/description', function (req, res, next) {
-    sendAnalyticsEvent("Install", `${req.query.name}/${req.query.version}`);
+    sendAnalyticsEvent(req, "Install", `${req.query.name}/${req.query.version}`);
 
     if (req.query.name == "brian-watkins/elmer" && req.query.version) {
       res.redirect(`/versions/${req.query.version}/elm-package.json`);
@@ -42,7 +57,7 @@ module.exports = function (elmer_versions) {
   });
 
   app.get('/all-packages', function (req, res, next) {
-    sendAnalyticsEvent("Install", "versions")
+    sendAnalyticsEvent(req, "Install", "versions")
 
     res.json(elmer_versions)
   });
